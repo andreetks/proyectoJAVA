0421017abd520abe8d8174ddd61fa26f06a14737
gonzalomuga <gonzalo.garcia@unmsm.edu.pe>
2019-11-04 22:11:00 -0500

    version 1.0

---
+++ src/Final/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Final;
+
+/**
+ * @author Gonzalo
+ */
+public class main {
+    
+    public static void main(String [] sqrt){
+        login ventana1 = new login();
+        menu menux = new menu();
+        ventana1.setVisible(true);
+        ventana1.setLocationRelativeTo(null);
+        menux.setLocationRelativeTo(null);
+    }
+    
+}
